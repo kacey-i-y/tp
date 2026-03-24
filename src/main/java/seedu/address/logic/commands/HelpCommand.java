5193c67ca5234c3b3c0dbf1418543452ee7daf64
felix <puahtzefoong@gmail.com>
2026-03-24 23:51:52 +0800

    Update help command

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
+import seedu.address.ui.HelpWindow;
 
 /**
  * Format full help instructions for every command for display.
@@ -12,10 +13,25 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE =
+            "Commands summary:\n"
+            + "------------------------------------------------------\n"
+            + "addathlete  n/NAME ag/AGE p/PHONE e/EMAIL a/ADDRESS sd/START_DATE [t/TAG]...\n"
+            + "addtiming   INDEX min/MINUTES sec/SECONDS\n"
+            + "deleteathlete  INDEX\n"
+            + "deletetiming   ATHLETE_INDEX TIMING_INDEX\n"
+            + "edit        INDEX [n/NAME] [ag/AGE] [p/PHONE] [e/EMAIL] [a/ADDRESS] [sd/START_DATE] [t/TAG]...\n"
+            + "find        KEYWORD [MORE_KEYWORDS]...\n"
+            + "sort        by/FIELD [order/ORDER]   (fields: name, pb  |  orders: asc, desc)\n"
+            + "viewathlete INDEX\n"
+            + "list\n"
+            + "clear\n"
+            + "exit\n"
+            + "------------------------------------------------------\n"
+            + "For full details: " + HelpWindow.USERGUIDE_URL;
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        return new CommandResult(SHOWING_HELP_MESSAGE, false, false);
     }
 }
