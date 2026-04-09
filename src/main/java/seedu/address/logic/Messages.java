0efcbc93602440fb08a3645f8b24efde06f817a2
Alexander Gerald <alex.gg.handoko@gmail.com>
2026-04-09 20:31:48 +0800

    Add validation checks to prefixes

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -15,6 +15,13 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_PHONE_NUMBER_FIND =
+        """
+        Invalid phone number format! Phone numbers should start with 9 or 8
+        """;
+    public static final String MESSAGE_EMPTY_PHONE_NUMBER = "You cannot enter an empty phone number!";
+    public static final String MESSAGE_EMPTY_PREFIX_VALUE = "The following prefix cannot have empty value: ";
+    public static final String MESSAGE_EMPTY_ARGUMENTS = "This command cannot have empty arguments!";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
